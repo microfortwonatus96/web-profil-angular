58ade67a375de4507013e1b16e5fd42a552418e7
yusuf <yusuf@cyberindo-sinergi.com>
2022-05-24 00:57:16 +0700

    update menu news

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,12 @@
-import { AfterViewInit, Component, OnInit, HostListener, Inject, Input, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  HostListener,
+  Inject,
+  Input,
+  Output,
+} from '@angular/core';
 import {
   ActivatedRoute,
   ActivationEnd,
@@ -8,53 +16,43 @@ import {
 import { LangList, LangService } from '../service/lang.service';
 declare var $: any;
 
-
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-
 export class HeaderComponent implements OnInit, AfterViewInit {
-  next: number = 1
-  @Input() careertext:any;
-  @Input() abouttext:any;
-  @Input() newstext:any;
+  next: number = 1;
+  @Input() careertext: any;
+  @Input() abouttext: any;
+  @Input() newstext: any;
 
   slider1 = true;
   slider2 = false;
-  
+
   constructor(
     private router: Router,
     public langService: LangService,
     private activatedRoute: ActivatedRoute
-  ) { }
+  ) {}
 
-  ngAfterViewInit(): void {
-  }
+  ngAfterViewInit(): void {}
   ngOnInit(): void {
-  //  this.loadHeader()
-  let openSideNav =  document.querySelector('.test')
-  openSideNav?.addEventListener('click', function (){
-    let sideNav = document.querySelector('.sidebar-nav')
-    sideNav?.classList.add('show-nav')
-    sideNav?.classList.remove('hide-nav')
-   
-  })
+    //  this.loadHeader()
+    let openSideNav = document.querySelector('.test');
+    openSideNav?.addEventListener('click', function () {
+      let sideNav = document.querySelector('.sidebar-nav');
+      sideNav?.classList.add('show-nav');
+      sideNav?.classList.remove('hide-nav');
+    });
 
-  // action news
-  let news  = document.querySelector('#nav');
-  news?.classList.remove('sticky-news');
-  news?.classList.add('nav');
+    // action news
   }
-  newsMenu(){
-    let stickyNews =  document.querySelector('.sticky-news')
-   
-    let nav = document.getElementById('nav');
-      nav?.classList.add('sticky-news')
-      nav?.classList.remove('nav')
-    
+  newsMenu() {
+    let nav = document.querySelector('#nav');
+    nav?.classList.add('sticky-news');
+    nav?.classList.remove('nav');
+
     let icon = document.querySelector('.icon-header-css');
     (<HTMLElement>icon).classList.add('logo2');
     (<HTMLElement>icon).classList.remove('logo1');
@@ -62,109 +60,106 @@ export class HeaderComponent implements OnInit, AfterViewInit {
     let toggle = document.querySelector('.icon-crumb');
     (<HTMLElement>toggle).classList.add('toggle-black');
     (<HTMLElement>toggle).classList.remove('icon-toggle');
-    let sideNav = document.querySelector('.sidebar-nav')
-    sideNav?.classList.remove('show-nav')
-    sideNav?.classList.add('hide-nav')
+    let sideNav = document.querySelector('.sidebar-nav');
+    sideNav?.classList.remove('show-nav');
+    sideNav?.classList.add('hide-nav');
   }
-  openMenu(){
-    let sideNav = document.querySelector('.sidebar-nav')
-    sideNav?.classList.add('show-nav')
-    sideNav?.classList.remove('hide-nav')
+  openMenu() {
+    let sideNav = document.querySelector('.sidebar-nav');
+    sideNav?.classList.add('show-nav');
+    sideNav?.classList.remove('hide-nav');
   }
-  closeSideNav(){
-    let sideNav = document.querySelector('.sidebar-nav')
-    let sideNav2 = document.querySelector('.sidenav2')
-    sideNav2?.classList.add('hide-nav')
-    sideNav2?.classList.remove('show-nav')
-    sideNav?.classList.add('hide-nav')
-    sideNav?.classList.remove('show-nav')
+  closeSideNav() {
+    let sideNav = document.querySelector('.sidebar-nav');
+    let sideNav2 = document.querySelector('.sidenav2');
+    setTimeout(() => {
+      sideNav2?.classList.add('hide-nav');
+      sideNav2?.classList.remove('show-nav');
+      sideNav?.classList.add('hide-nav');
+      sideNav?.classList.remove('show-nav');
+    }, 100);
   }
   loadHeader() {
-      let scrol =  document.querySelector('.mat-typography');
-      let header = document.querySelector('#nav') 
-      let icon = document.querySelector('.icon-header-css');
-      let news = document.querySelector('.sticky-news');
-    
-      if(news) return
-     (<HTMLElement>scrol).addEventListener('scroll', function() {
-      let toggle = document.querySelector('.icon-crumb');
-      let menu = document.querySelector('.menu');
-      const scrolled = (<HTMLElement>scrol).scrollTop;
-       if(scrolled > 710 ){
-        
-        console.log("before", menu);
-        (<HTMLElement>header).classList.remove('nav'); 
-        (<HTMLElement>header).classList.add('sticky');
-           
-        (<HTMLElement>icon).classList.add('logo2');
-        (<HTMLElement>icon).classList.remove('logo1');
+    let scrol = document.querySelector('.mat-typography');
+    let header = document.querySelector('#nav');
+    let icon = document.querySelector('.icon-header-css');
+    let news = document.querySelector('.sticky-news');
 
-        (<HTMLElement>toggle).classList.add('toggle-black');
-        (<HTMLElement>toggle).classList.remove('icon-toggle');     
-  
-       }else{
-        (<HTMLElement>header).classList.add('nav');
-        (<HTMLElement>header).classList.remove('sticky');
+    if (news) return;
+    (<HTMLElement>scrol).addEventListener(
+      'scroll',
+      function () {
+        let toggle = document.querySelector('.icon-crumb');
+        let menu = document.querySelector('.menu');
+        const scrolled = (<HTMLElement>scrol).scrollTop;
+        if (scrolled > 710) {
+          (<HTMLElement>header).classList.remove('nav');
+          (<HTMLElement>header).classList.add('sticky');
 
-        (<HTMLElement>icon).classList.remove('logo2');
-        (<HTMLElement>icon).classList.add('logo1');
+          (<HTMLElement>icon).classList.add('logo2');
+          (<HTMLElement>icon).classList.remove('logo1');
 
-        (<HTMLElement>toggle).classList.remove('toggle-black');
-        (<HTMLElement>toggle).classList.add('icon-toggle');
-        
-       }
-      
-      }, false);
+          (<HTMLElement>toggle).classList.add('toggle-black');
+          (<HTMLElement>toggle).classList.remove('icon-toggle');
+        } else {
+          (<HTMLElement>header).classList.add('nav');
+          (<HTMLElement>header).classList.remove('sticky');
+
+          (<HTMLElement>icon).classList.remove('logo2');
+          (<HTMLElement>icon).classList.add('logo1');
+
+          (<HTMLElement>toggle).classList.remove('toggle-black');
+          (<HTMLElement>toggle).classList.add('icon-toggle');
+        }
+      },
+      false
+    );
   }
- 
-  show(){
-    let c = document.querySelector('.header2');
-    if(this.next > 1){
-      console.log(this.next);
-      this.next--;
-      c?.classList.remove('show')
-      c?.classList.add('hide')
 
-    }else {
-      console.log(this.next);
+  show() {
+    let c = document.querySelector('.header2');
+    if (this.next > 1) {
+      this.next--;
+      c?.classList.remove('show');
+      c?.classList.add('hide');
+    } else {
       this.next++;
-      c?.classList.add('show')
-      c?.classList.remove('hide')
+      c?.classList.add('show');
+      c?.classList.remove('hide');
     }
   }
-  showSideNav(){
-    let sideNav = document.querySelector('.sidebar-nav')
-    let sideNav2 = document.querySelector('.sidenav2')
-    sideNav?.classList.add('hide-nav')
-    sideNav2?.classList.add('show-nav')
-    sideNav2?.classList.remove('hide-nav')
+  showSideNav() {
+    let sideNav = document.querySelector('.sidebar-nav');
+    let sideNav2 = document.querySelector('.sidenav2');
+    sideNav?.classList.add('hide-nav');
+    sideNav2?.classList.add('show-nav');
+    sideNav2?.classList.remove('hide-nav');
   }
 
-  
-  click1(){
-    let sld1 = document.querySelector('.display-block')
+  click1() {
+    let sld1 = document.querySelector('.display-block');
     let slider1 = document.querySelector('.paralax-section1');
     let slider2 = document.querySelector('.paralax-section2');
 
-    if(sld1){
+    if (sld1) {
       slider2?.classList.remove('display-block');
       slider2?.classList.add('display-none');
       slider1?.classList.add('display-block');
-    }else {
+    } else {
       slider2?.classList.add('display-none');
       slider1?.classList.add('display-block');
     }
   }
-  click2(){
+  click2() {
     let slider1 = document.querySelector('.paralax-section1');
     let slider2 = document.querySelector('.paralax-section2');
     let sld2 = document.querySelector('.display-block');
 
-    if(sld2){
+    if (sld2) {
       slider1?.classList.remove('display-block');
       slider1?.classList.add('display-none');
       slider2?.classList.add('display-block');
-    }else {
+    } else {
       slider1?.classList.add('display-none');
       slider2?.classList.add('display-block');
     }
--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,32 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, ActivationEnd, NavigationEnd, Router,} from '@angular/router';
+import {
+  ActivatedRoute,
+  ActivationEnd,
+  NavigationEnd,
+  Router,
+} from '@angular/router';
 import { LangList, LangService } from '../service/lang.service';
 declare var $: any;
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.scss']
+  styleUrls: ['./news.component.scss'],
 })
 export class NewsComponent implements OnInit {
-  newsHeader:any = {
+  newsHeader: any = {
     highlight: 'HIGHLIGHTS',
     date: '00 October 2021',
     title: 'This is Highlight News Title Lorem Ipsum Dolor Sit Amet',
     hastag: '#Technology #Finance #hashtag3 #hashtag4',
-    slide: ''
-  }
+    slide: '',
+  };
   // slider1 = true;
   // slider2 = false;
   newsUpdate = false;
   newsSlider = false;
-  
+
   constructor(
     private router: Router,
     public langService: LangService,
     private activatedRoute: ActivatedRoute
-  ) { 
-//  this.loadNews()
+  ) {
+    // this.loadNews();
   }
 
   ngOnInit(): void {
@@ -37,31 +42,35 @@ export class NewsComponent implements OnInit {
       'slow'
     );
 
-  // this.loadNews()
-  let news  = document.getElementById('nav');
-  news?.classList.remove('nav')
-  news?.classList.add('sticky-news')
+    this.loadNews();
+    setTimeout(() => {
+      let news = document.querySelector('#nav');
+      news?.classList.remove('nav');
+      news?.classList.add('sticky-news');
+      let icon = document.querySelector('.icon-header-css');
+      (<HTMLElement>icon).classList.add('logo2');
+      (<HTMLElement>icon).classList.remove('logo1');
+      let toggle = document.querySelector('.icon-crumb');
+      (<HTMLElement>toggle).classList.add('toggle-black');
+      (<HTMLElement>toggle).classList.remove('icon-toggle');
+    }, 100);
   }
-  loadNews(){
-    
-  
-
+  loadNews() {
     let scrol = document.querySelector('.menu-news');
     scrol?.addEventListener('click', function () {
-    let header = document.getElementById('nav');
-    let icon = document.querySelector('.icon-header-css');
-    let toggle = document.querySelector('.icon-crumb');
-   
-    header?.classList.add('sticky-news');
-    header?.classList.remove('sticky');
-    header?.classList.remove('nav');
-    
-    icon?.classList.add('logo2');
-    // icon?.classList.remove('logo1');
-    
-    toggle?.classList.add('toggle-black');
-    toggle?.classList.remove('icon-toggle'); 
-    }
-    );
+      let header = document.getElementById('nav');
+      let icon = document.querySelector('.icon-header-css');
+      let toggle = document.querySelector('.icon-crumb');
+
+      header?.classList.add('sticky-news');
+      header?.classList.remove('sticky');
+      header?.classList.remove('nav');
+
+      icon?.classList.add('logo2');
+      // icon?.classList.remove('logo1');
+
+      toggle?.classList.add('toggle-black');
+      toggle?.classList.remove('icon-toggle');
+    });
   }
 }
